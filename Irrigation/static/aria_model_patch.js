ef027e3d4e9060f297a3e0cfc6ca9205ae869961
frublev <f.rublev@gmail.com>
2024-12-22 14:42:34 +0100

    alfa for rasp pi (edd err)

--- Irrigation/static/aria_model_patch.js
+++ Irrigation/static/aria_model_patch.js
@@ -1,15 +1,32 @@
 
 function radio_check(elbyname) {
   let elements = document.getElementsByName(elbyname);
+  let disabled_elements = document.getElementsByName('dis');
   for (let i = 0; i < elements.length; i++) {
     let lab = 'check_' + elements[i].id + '';
     if (elements[i].value == "True") {
       elements[i].checked = true;
       document.getElementById(lab).innerText = 'On';
+      if (elbyname == 'area_auto') {
+        for (let i = 0; i < disabled_elements.length; i++) {
+            disabled_elements[i].disabled = true;
+        }
+      }
     }
     else {
       elements[i].checked = false;
       document.getElementById(lab).innerText = 'Off';
+      if (elbyname == 'area_status') {
+        for (let i = 0; i < disabled_elements.length; i++) {
+            disabled_elements[i].disabled = true;
+        }
+        let ar_id = elements[i].id;
+        ar_id = ar_id.indexOf('off');
+        ar_id = ar_id + 3;
+        ar_id = elements[i].id.slice(ar_id)
+        let el_id = 'area_auto' + ar_id
+        document.getElementById(el_id).disabled = true
+      }
     }
   }
 }
@@ -19,6 +36,7 @@ const area_status = async (area_ = '', radio = '') => {
   let area_id = 'area_' + radio + area_;
   let status = document.getElementById(area_id).value;
   let lab_ = 'check_area_' + radio + area_;
+  let disabled_ = document.getElementsByName('dis');
   let data = {}
 
   if (status == "True") {
@@ -47,13 +65,39 @@ const area_status = async (area_ = '', radio = '') => {
     body: JSON.stringify(data)
   });
   let area_status = await response.json();
+  let el_id_ = 'area_auto' + area_;
   if (typeof area_status[radio] == "boolean" && area_status[radio] == true) {
     document.getElementById(area_id).value = "True";
     document.getElementById(lab_).innerText = "On";
+    if (radio == 'on_off') {
+        if (document.getElementById(el_id_).value != "True") {
+            for (let i = 0; i < disabled_.length; i++) {
+                disabled_[i].disabled = false;
+            }
+        }
+        document.getElementById(el_id_).disabled = false;
+    }
+    if (radio == 'auto'){
+        for (let i = 0; i < disabled_.length; i++) {
+            disabled_[i].disabled = true;
+        }
+    }
+
   }
   else if (typeof area_status[radio] == "boolean" && area_status[radio] == false) {
     document.getElementById(area_id).value = "False";
     document.getElementById(lab_).innerText = "Off";
+    if (radio == 'on_off') {
+        document.getElementById(el_id_).disabled = true;
+        for (let i = 0; i < disabled_.length; i++) {
+            disabled_[i].disabled = true;
+        }
+    }
+    if (radio == 'auto'){
+        for (let i = 0; i < disabled_.length; i++) {
+            disabled_[i].disabled = false;
+        }
+    }
   }
   return;
 }
--- Irrigation/weather_stat.py
+++ Irrigation/weather_stat.py
@@ -1,8 +1,15 @@
+import logging
+
 import os
 from datetime import datetime
 import json
 import requests
 
+from .loggers import log_handler
+
+weather_logger = logging.getLogger(__name__)
+weather_logger.setLevel(logging.INFO)
+weather_logger.addHandler(log_handler)
 
 w_path = os.path.join(os.path.dirname(__file__), 'cash/forecast.json')
 print('PATH!!!!!!!!', w_path)
@@ -28,8 +35,16 @@ def get_forecast(coordinates):
                             f'wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant&'
                             f'current=temperature_2m,relative_humidity_2m,rain,showers,snowfall,cloud_cover,'
                             f'surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m')
-    with open(w_path, 'w') as f:
-        json.dump(response.json(), f)
+    if response:
+        with open(w_path, 'w') as f:
+            try:
+                json.dump(response.json(), f)
+            except requests.exceptions.JSONDecodeError:
+                msg = 'Error dumping to json'
+                weather_logger.error(msg)
+    else:
+        msg = 'Weather forecast is empty'
+        weather_logger.error(msg)
     return True
 
 
@@ -47,24 +62,54 @@ def set_sunrise():
     return sun_rise_time, ct
 
 
-def get_weather():
+def wind_dir_str(wind_dir):
+    if 22.5 <= wind_dir < 67.5:
+        w_dir_str = 'SW'
+    elif 67.5 <= wind_dir < 112.5:
+        w_dir_str = 'W'
+    elif 112.5 <= wind_dir < 157.5:
+        w_dir_str = 'NW'
+    elif 157.5 <= wind_dir < 202.5:
+        w_dir_str = 'N'
+    elif 202.5 <= wind_dir < 247.5:
+        w_dir_str = 'NE'
+    elif 247.5 <= wind_dir < 292.5:
+        w_dir_str = 'E'
+    elif 292.5 <= wind_dir < 337.5:
+        w_dir_str = 'SE'
+    else:
+        w_dir_str = 'S'
+    return w_dir_str
+
+
+def get_weather(t='base'):
     with open(w_path) as w_file:
         weather = json.load(w_file)
-    temperature = [str(round(weather['current']["temperature_2m"])) + '\u00A0' + '°C']
     next_hour = datetime.now()
     next_hour = next_hour.hour
-    temp_for = [weather['hourly']['temperature_2m'][next_hour:25], weather['hourly']['temperature_2m'][25:]]
-    for t in temp_for:
-        t.sort()
-        t_min, t_max = t[0], t[-1]
-        if t_min == t_max:
-            temperature.append(str(round(t_min)) + '\u00A0' + '°C')
-        else:
-            temperature.append(str(round(t_min)) + '...' + str(round(t_max)) + '\u00A0' + '°C')
-    return temperature
+    if t == 'base':
+        temperature = [str(round(weather['current']["temperature_2m"])) + '\u00A0' + '°C']
+        temp_for = [weather['hourly']['temperature_2m'][next_hour:25], weather['hourly']['temperature_2m'][25:]]
+        for t in temp_for:
+            t.sort()
+            t_min, t_max = t[0], t[-1]
+            if t_min == t_max:
+                temperature.append(str(round(t_min)) + '\u00A0' + '°C')
+            else:
+                temperature.append(str(round(t_min)) + '...' + str(round(t_max)) + '\u00A0' + '°C')
+        return temperature
+    elif t == 'now':
+        wind = wind_dir_str(weather['current']["wind_direction_10m"])
+        weather_data = {'time': weather['current']['time'][-5:],
+                        'temperature': str(weather['current']["temperature_2m"]) + " \u00b0C",
+                        'humidity': str(weather['current']["relative_humidity_2m"]) + " %",
+                        'surface_pressure': str(weather['current']["surface_pressure"]) + 'hPa',
+                        'wind': str(wind)}
+        return weather_data
 
 
 if __name__ == '__main__':
     a = get_forecast(COORD)
-    sun, check_time = set_sunrise()
-    print(sun)
+    # sun, check_time = set_sunrise()
+    b = get_weather('now')
+    print(b)
